18bd944c6c2f51a18f5cde499f81d127745281aa
Joabe de jesus santos <jb_santos@Joabes-MacBook-Pro.local>
2024-08-20 09:29:14 -0300

    teste 2

--- App.tsx
+++ App.tsx
@@ -4,16 +4,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {GlobalStateProvider} from './src/contexts/GlobalContext';
 import {MainNavigation} from './src/navigation/Navigation';
 
-export { MainNavigation }
+const App = () => {
+  return (
+    <NavigationContainer>
+      <GlobalStateProvider>
+        <MainNavigation />
+      </GlobalStateProvider>
+    </NavigationContainer>
+  );
+};
 
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <GlobalStateProvider>
-//         <MainNavigation />
-//       </GlobalStateProvider>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default App;
+export default App;
